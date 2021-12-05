d0fcac8dc77644f61cb64c2a20ce8de303932b38
Chintu <bcsf18m017@pucit.edu.pk>
2021-12-05 15:47:12 +0500

    Image Picker Implemented

--- app/src/main/java/com/example/actionbarpractice/MainActivity.java
+++ app/src/main/java/com/example/actionbarpractice/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.actionbarpractice;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,14 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imageView=findViewById(R.id.imageView);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,15 +41,37 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     break;
             case R.id.action_favorite:
-                    Toast.makeText(getApplicationContext(),"Fav", Toast.LENGTH_LONG).show();
-                    break;
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+
+                break;
             case R.id.action_tasks:
-                    TextView textView = findViewById(R.id.textView3);
-                    textView.setText("Action task was clicked");
+                Toast.makeText(this, "Tasks", Toast.LENGTH_SHORT).show();
                     break;
                 default:
                     return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            if (selectedImage != null) {
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                if (cursor != null) {
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                    cursor.close();
+                }
+            }
+
+        }
     }
 }
