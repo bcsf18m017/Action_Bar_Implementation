2e3df8239713f5d4fe21090275cf0c93ef408320
Chintu <bcsf18m017@pucit.edu.pk>
2021-12-05 15:17:12 +0500

    Menu Java Code Implemented

--- app/src/main/java/com/example/actionbarpractice/MainActivity.java
+++ app/src/main/java/com/example/actionbarpractice/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.actionbarpractice;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_settings:
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+            case R.id.action_favorite:
+                    Toast.makeText(getApplicationContext(),"Fav", Toast.LENGTH_LONG).show();
+                    break;
+            case R.id.action_tasks:
+                    TextView textView = findViewById(R.id.textView3);
+                    textView.setText("Action task was clicked");
+                    break;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
